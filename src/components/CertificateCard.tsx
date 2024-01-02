87b3912fd285c19303cbc767d4d12934b72a620f
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-02 14:59:53 +0530

    Recent certifications added

--- src/components/CertificateCard.tsx
+++ src/components/CertificateCard.tsx
@@ -7,6 +7,7 @@ const CertificateCard = ({
   alt,
   description,
   publisher,
+  certificateDetails,
 }: certificate) => {
   return (
     <div
@@ -18,12 +19,19 @@ const CertificateCard = ({
       </a>
       <div className="p-3 border-t-2 border-pink-500">
         <a href={url} target="_blank">
-          <h3 className="mb-2 text-[16px] md:text-2xl font-bold tracking-tight text-pink-500 p-1 duration-100 ease-in-out group-hover:text-pink-700 group-hover:border-b-[1px] group-hover:border-b-white">
-            {name}
+          <h3 className="mb-2 text-[16px] md:text-2xl font-bold tracking-tight text-pink-500 p-1">
+            <span className="duration-200 ease-in-out group-hover:text-pink-700 group-hover:border-b-[1px] group-hover:border-b-white">
+              {name}
+            </span>
           </h3>
-          <h5 className="mb-2 text-[14px] md:text-xl">
-            Offered by : {publisher}
-          </h5>
+          <a href={certificateDetails} target="_blank">
+            <h5 className="mb-2 text-[14px] md:text-xl">
+              Offered by :{" "}
+              <span className="duration-75 ease-in-out hover:text-pink-500 hover:border-b-[1px] hover:border-b-white">
+                {publisher}
+              </span>
+            </h5>
+          </a>
         </a>
         <p className="mb-2 font-normal p-3 text-[15px] md:text-xl">
           {description}
